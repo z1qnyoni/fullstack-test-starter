f414e382081924c6f40399c92f0ac985ee971771
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2023-03-02 14:44:04 +0200

    Added router and graphql. Added boilerplate code. It seems autoloading does not work as VSCode throws errors

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->post('/graphql', App\Controller\GraphQL::handle);
+});
+
+$routeInfo = $dispatcher->dispatch(
+    $_SERVER['REQUEST_METHOD'],
+    $_SERVER['REQUEST_URI']
+);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        echo $handler($vars);
+        break;
+}
